49d1df1ee7692ff8f8f4fd746ae68133569500fa
Omoniyi Opemipo [Electron] <omoniyiopempo6@gmail.com>
2024-04-03 16:02:29 +0100

    Designed Contact Section, fully responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import TopBanner from "@/components/TopBanner/TopBanner";
 import About from "@/components/About/About";
 import Skills from "@/components/Skills/Skills";
 import Projects from "@/components/Projects/Projects";
+import Contact from "@/components/Contact/Contact";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
           
           <div id="home-content" className="py-28 grid grid-rows-4 gap-2 px-2 sm:grid sm:grid-cols-3">
             <h1 className="text-black font-extrabold bg-transparent row-span-2 self-end text-center text-4xl sm:text-white sm:self-start sm:col-span-2 sm:row-span-1 sm:drop-shadow-2xl md:text-6xl">Omoniyi Opemipo</h1>
-            <h2 className="text-black bg-transparent text-center sm:text-white sm:text-start sm:col-span-2 sm:justify-self-center shadow-black sm:drop-shadow-2xl">Web Developer & Designer</h2>
+            <h2 className="text-black bg-transparent text-center sm:text-white sm:text-start sm:col-span-2 sm:justify-self-center shadow-black sm:text-4xl sm:drop-shadow-2xl">Web Developer & Designer</h2>
 
             <button className="button"><Link href={"#"}>Contact Me</Link></button>
           </div>
@@ -21,6 +22,7 @@ export default function Home() {
         <About />
         <Skills />
         <Projects />
+        <Contact />
     </main>
   );
 }
