93be5719e5b4194c00cee300027711c292532c2b
daniel_eln <omoniyiopemipo6@gmail.com>
2024-12-21 00:29:36 +0100

    fix(app): solved deployment error caused by apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <div id="home-content" className="flex flex-col justify-between gap-6 pt-24 items-center p-10 px-0 bg-black md:gap-10 md:pt-36 lg:pt-[11.6rem] lg:py-[4.1rem] lg:flex-row lg:gap-0 md:px-16">
             <div className="text-content flex flex-col gap-4 text-white px-8 leading-6">
               <h1 className="text-4xl font-bold md:text-6xl">Hello, there 👋</h1>
-              <h2 className="text-xl font-poppins md:text-3xl">I'm <span className="text-yellow-500 text-bold">Opemipo Omoniyi (ELECTRON)</span></h2>
+              <h2 className="text-xl font-poppins md:text-3xl">I&apos;m <span className="text-yellow-500 text-bold">Opemipo Omoniyi (ELECTRON)</span></h2>
               <p className="font-poppins leading-[32px] sm:text-justify">
                 Frontend Developer with 3 years of experience in developing web applications from ideation to designs and to 
                 live applications using technologies like React Js, Typescript, Javascript, HTML, CSS and so on. Skilled at writing 
