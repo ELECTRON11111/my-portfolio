57b36f66fc6a8f6746e98a00f7db4a76525b8e47
daniel_eln <omoniyiopemipo6@gmail.com>
2025-12-10 19:01:52 +0100

    feat: add to more skills to section

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -4,15 +4,16 @@ export default function Skills() {
             <h2 className="text-xl font-extrabold py-4 sm:col-span-2 sm:justify-self-center sm:text-center">Skills.</h2>
             <p className="sm:col-span-2 sm:text-center">Over the years, I&apos;ve developed core programming skills for problem solving that has further improved the 
             quality and efficiency of my web applications and scripts. Also understanding the web, its nature and optimizations that can be made to improve SEO results have been a great
-            addition to my skill set. My focus and some of the technologies I&apos;ve worked with are shown below :</p>
+            addition to my skill set. My focus and some of the core technologies I&apos;ve worked with are shown below :</p>
 
             <div id="focus" className="py-4 flex flex-col gap-2 justify-between sm:p-6">
                 <h3 className="px-6 py-2 my-2 border-b-2 inline-block">MY FOCUS</h3>
 
                 <h3>Solving Business Problems</h3>
-                <h3>User Experience (UX) Optimization</h3>
-                <h3>Search Engine Optimization</h3>
                 <h3>Web Accessibility</h3>
+                <h3>Web Performance Optimization</h3>
+                <h3>User Experience (UX) Optimization</h3>
+                <h3>Search Engine Optimization (SEO)</h3>
                 <h3>Responsive Design</h3>
             </div>
 
@@ -30,18 +31,25 @@ export default function Skills() {
                         <div className="bg-yellow-500 basis-[80%] text-yellow-500">.</div>
                     </div>
                 </div>
-                
+
                 <div id="technology" className="w-full h-10 bg-neutral-900 flex items-center">
-                    <span className="block p-4 w-[6.5rem]">JavaScript</span>
+                    <span className="block p-4 w-[6.5rem] pr-14">SCSS</span>
                     <div className="bg-white basis-3/4 flex">
                         <div className="bg-yellow-500 basis-[80%] text-yellow-500">.</div>
                     </div>
                 </div>
                 
                 <div id="technology" className="w-full h-10 bg-neutral-900 flex items-center">
+                    <span className="block p-4 w-[6.5rem]">JavaScript</span>
+                    <div className="bg-white basis-3/4 flex">
+                        <div className="bg-yellow-500 basis-[90%] text-yellow-500">.</div>
+                    </div>
+                </div>
+                
+                <div id="technology" className="w-full h-10 bg-neutral-900 flex items-center">
                     <span className="block p-4 w-[6.5rem]">Typescript</span>
                     <div className="bg-white basis-3/4 flex">
-                        <div className="bg-yellow-500 basis-[80%] text-yellow-500">.</div>
+                        <div className="bg-yellow-500 basis-[90%] text-yellow-500">.</div>
                     </div>
                 </div>
                 
@@ -62,7 +70,7 @@ export default function Skills() {
                 <div id="technology" className="w-full h-10 bg-neutral-900 flex items-center">
                     <span className="block p-4 w-[6.5rem] pr-14">NextJs</span>
                     <div className="bg-white basis-3/4 flex">
-                        <div className="bg-yellow-500 basis-3/4 text-yellow-500">.</div>
+                        <div className="bg-yellow-500 basis-[85%] text-yellow-500">.</div>
                     </div>
                 </div>
             </div>
